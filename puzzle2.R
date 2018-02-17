1d6b1d1e44e482fcb6dee28e5e109ac9b2130e20
Dana Seidel <dpseidel@berkeley.edu>
2018-02-17 00:18:40 -0800

    puzzle 2 part 2

--- puzzle2.R
+++ puzzle2.R
@@ -7,3 +7,33 @@ input <- read_tsv("day2input.txt", col_names = FALSE)
 diff = apply(input, 1, max) - apply(input, 1, min) 
 checksum = sum(diff)
 checksum
+
+
+## part 2
+
+test <- matrix(nrow=3, ncol=4, c(5, 9, 2, 8, 9, 4, 7, 3, 3, 8, 6, 5), byrow=T)
+test
+
+ans <- c(4,3,2)
+
+is.wholenumber <- function(x, tol = .Machine$double.eps^0.5){
+  abs(x - round(x)) < tol
+  }
+
+divisible <- function(vector){
+  for(i in seq(vector)){
+   modulus <- (vector[i] %% vector)
+   if(sum(modulus==0) == 2){
+     pair = which(modulus==0)
+     other = subset(pair, pair != i)
+     quotients = c(vector[i]/vector[other], vector[other]/vector[i])
+     answer = quotients[which(is.wholenumber(quotients))]
+     return(ifelse(length(answer) == 1, answer, max(quotients)))
+   }
+  }
+  }
+
+apply(test, 1, divisible) == ans
+
+apply(input, 1, divisible) %>% sum
+#282
